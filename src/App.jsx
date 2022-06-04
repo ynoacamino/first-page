fca35ca65bc75e75c72495c452853298ec3c0f3a
Diegoo11 <ynoacamino@gmail.com>
2022-06-04 11:39:25 -0500

    added react router doom config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
+import Slider from './Slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <br />
+      <Slider />
     </div>
   );
 }
