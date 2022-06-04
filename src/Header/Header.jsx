ce7502d625ce1674eb4fd3fd4eebba1230694926
Diegoo11 <ynoacamino@gmail.com>
2022-06-04 11:42:23 -0500

    separacion de .Nav del .Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,18 +1,49 @@
 import './Header.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 import { faSlack } from '@fortawesome/free-brands-svg-icons';
 import BtnBurger from '../BtnBurger/BtnBurger';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
+  const clickOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="Header">
       <a className="homeHeader link" href="/">
         <FontAwesomeIcon icon={faSlack} size="3x" />
         <span className="titleHeader">Eshop</span>
       </a>
-      <ul className="Nav">
+      <div className="rigth">
+        <ul className={`Nav ${open && 'NavView'}`}>
+          <li className="li">
+            <a className="link navLink underline" href="/">Inicio</a>
+          </li>
+          <li className="li">
+            <a className="link navLink underline" href="/">Productos</a>
+          </li>
+          <li className="li">
+            <a className="link navLink underline" href="/">Marcas</a>
+          </li>
+          <li className="li">
+            <a className="link navLink underline" href="/">Tiendas</a>
+          </li>
+          <li className="li">
+            <a className="link navLink underline" href="/">Comprar</a>
+          </li>
+        </ul>
+        <button type="button" className="btnSearchNav pointer">
+          <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
+        </button>
+        <button type="button" className="btnSearchNav" onClick={clickOpen}>
+          <BtnBurger open={open} />
+        </button>
+      </div>
+      <ul className={`NavMobile ${open && 'NavMobileView'}`}>
         <li className="li">
           <a className="link navLink underline" href="/">Inicio</a>
         </li>
@@ -28,10 +59,9 @@ function Header() {
         <li className="li">
           <a className="link navLink underline" href="/">Comprar</a>
         </li>
-        <button type="button" className="btnSearchNav pointer">
-          <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
+        <button type="button" className="btnExit pointer" onClick={clickOpen}>
+          <FontAwesomeIcon icon={faX} size="2x" />
         </button>
-        <BtnBurger />
       </ul>
     </div>
   );
