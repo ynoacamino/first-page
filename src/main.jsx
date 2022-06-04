553a12abddaf8beb0e4b8da02ab4d9781e59cfda
Diegoo11 <ynoacamino@gmail.com>
2022-06-04 11:40:47 -0500

    config for react-router-dom added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// eslint-disable-next-line import/order
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
