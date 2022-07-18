cca0b705525de861e19d254e29f607fa8dddf444
Diegoo11 <ynoacamino@gmail.com>
2022-07-18 12:34:51 -0500

    deleted components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 // import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './Header/Header';
 import Slider from './Slider/Slider';
 import Background from './Background/Background';
-import Subscribe from './Subscribe/Subscribe';
-import Footer from './Footer/Footer';
 import PrimaryProducts from './PrimaryProducts/PrimaryProducts';
+import SecundaryProducts from './SecundaryProducts/SecundaryProducts';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Background />
       <Slider />
       <PrimaryProducts />
-      <PrimaryProducts />
-      <Subscribe />
-      <Footer />
+      <SecundaryProducts />
     </div>
   );
 }
