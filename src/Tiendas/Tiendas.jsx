ecb64bc92d1930a9dfeb1fcef51a4fde2875a6da
Diegoo11 <ynoacamino@gmail.com>
2022-07-18 14:10:15 -0500

    new file for component

---
+++ src/Tiendas/Tiendas.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tiendas.css';
+
+function Tiendas() {
+  return (
+    <div className="Tiendas">
+      <h1 className="titleTienda">
+        CONOCE NUESTRAS TIENDAS
+      </h1>
+      <h2>Arequipa</h2>
+    </div>
+  );
+}
+
+export default Tiendas;
