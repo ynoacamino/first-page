f0b8ef7cea2fb92406384ba9aa1cbd0514cb3116
Diegoo11 <ynoacamino@gmail.com>
2022-07-18 12:35:09 -0500

    use react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,19 @@ import App from './App';
 import './index.css';
 // eslint-disable-next-line import/order
 import { BrowserRouter } from 'react-router-dom';
+import Header from './Header/Header';
+import Subscribe from './Subscribe/Subscribe';
+import Footer from './Footer/Footer';
 
 const store = configureStore({ reducer: {} });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
+      <Header />
       <App />
+      <Subscribe />
+      <Footer />
     </BrowserRouter>
   </Provider>
   ,
