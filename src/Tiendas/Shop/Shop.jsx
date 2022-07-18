2e4cff183d05f65eedbd563aefa857420c103fd5
Diegoo11 <ynoacamino@gmail.com>
2022-07-18 18:34:35 -0500

    change a components moduler

--- src/Tiendas/Shop/Shop.jsx
+++ src/Tiendas/Shop/Shop.jsx
@@ -3,34 +3,36 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faEnvelope, faClock } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import Hr from '../Hr/Hr';
 
-function Shop() {
+function Shop({
+  name, direccion, phone, email, horario,
+}) {
   return (
     <div className="Shop">
-      <div className="boxImgShop">
-        <img
-          src="https://xiaomiperu.com/media/direccion_tienda_mi_store_plaza_norte_xiaomi.png"
-          alt="shop"
-        />
-      </div>
+      <img
+        src="https://xiaomiperu.com/media/direccion_tienda_mi_store_plaza_norte_xiaomi.png"
+        alt="shop"
+        className="boxImgShop"
+      />
       <div className="boxTextShop">
-        <span className="nameShop">Xiaomi Plaza norte</span>
+        <span className="nameShop">{name}</span>
         <div className="infoShop">
           <div className="guionShop">
             <FontAwesomeIcon icon={faHouse} />
-            Calle Schell 168, frente al parque Keneddy
+            {direccion}
           </div>
           <div className="guionShop">
             <FontAwesomeIcon icon={faWhatsapp} />
-            +51 932 262 031
+            {phone}
           </div>
           <div className="guionShop">
             <FontAwesomeIcon icon={faEnvelope} />
-            informes@xiaomiperu.com
+            {email}
           </div>
           <div className="guionShop">
             <FontAwesomeIcon icon={faClock} />
-            Lunes a Sábado: 10:00 - 21:00
+            {horario}
           </div>
         </div>
       </div>
