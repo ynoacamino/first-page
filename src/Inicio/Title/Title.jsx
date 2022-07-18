e1a3243585916d774a7ec05655da19f4c1b833ec
Diegoo11 <ynoacamino@gmail.com>
2022-07-18 14:10:12 -0500

    new file for component

---
+++ src/Inicio/Title/Title.jsx
@@ -0,0 +1,27 @@
+import './Title.css';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const word = 'ESHOP';
+
+function Title() {
+  return (
+    <div className="Title">
+      {word
+        .split('')
+        .map((lether, index) => (
+          <motion.div
+            animate={{ y: 50, opacity: 1 }}
+            initial={{ y: 350, opacity: 0 }}
+            transition={{ delay: index / 3 + 0.5 }}
+            key={lether}
+            className="boxLether"
+          >
+            {lether}
+          </motion.div>
+        ))}
+    </div>
+  );
+}
+
+export default Title;
