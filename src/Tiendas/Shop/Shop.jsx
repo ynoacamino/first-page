428329cbda29b5e01900f3b2ff89a9199efc4475
Diegoo11 <ynoacamino@gmail.com>
2022-07-19 11:35:20 -0500

    add component condicional

--- src/Tiendas/Shop/Shop.jsx
+++ src/Tiendas/Shop/Shop.jsx
@@ -6,37 +6,40 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import Hr from '../Hr/Hr';
 
 function Shop({
-  name, direccion, phone, email, horario,
+  name, direccion, phone, email, horario, vol,
 }) {
   return (
-    <div className="Shop">
-      <img
-        src="https://xiaomiperu.com/media/direccion_tienda_mi_store_plaza_norte_xiaomi.png"
-        alt="shop"
-        className="boxImgShop"
-      />
-      <div className="boxTextShop">
-        <span className="nameShop">{name}</span>
-        <div className="infoShop">
-          <div className="guionShop">
-            <FontAwesomeIcon icon={faHouse} />
-            {direccion}
-          </div>
-          <div className="guionShop">
-            <FontAwesomeIcon icon={faWhatsapp} />
-            {phone}
-          </div>
-          <div className="guionShop">
-            <FontAwesomeIcon icon={faEnvelope} />
-            {email}
-          </div>
-          <div className="guionShop">
-            <FontAwesomeIcon icon={faClock} />
-            {horario}
+    <>
+      <div className="Shop">
+        <img
+          src="https://xiaomiperu.com/media/direccion_tienda_mi_store_plaza_norte_xiaomi.png"
+          alt="shop"
+          className="boxImgShop"
+        />
+        <div className="boxTextShop">
+          <span className="nameShop">{name}</span>
+          <div className="infoShop">
+            <div className="guionShop">
+              <FontAwesomeIcon icon={faHouse} />
+              {direccion}
+            </div>
+            <div className="guionShop">
+              <FontAwesomeIcon icon={faWhatsapp} />
+              {phone}
+            </div>
+            <div className="guionShop">
+              <FontAwesomeIcon icon={faEnvelope} />
+              {email}
+            </div>
+            <div className="guionShop">
+              <FontAwesomeIcon icon={faClock} />
+              {horario}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {vol && <Hr />}
+    </>
   );
 }
 
