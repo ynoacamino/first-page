2ab070a20c59735a65523715ac4cbad140305465
Diegoo11 <ynoacamino@gmail.com>
2022-07-22 14:15:40 -0500

    new component

---
+++ src/Marcas/Marcas.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './Marcas.css';
+
+function Marcas() {
+  return (
+    <div className="Marcas">
+      <div className="titleBoxMarcas">
+        <div className="degradedMarcas" />
+        <div className="titleBoxText">
+          <span>MARCAS</span>
+        </div>
+        <img
+          src="https://homeofficehacks.com/wp-content/uploads/2022/04/gaming-space.jpg"
+          alt=""
+          className="imgMarcas"
+        />
+      </div>
+      <div className="boxMarcasImg">
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+        <img
+          src="https://www.tiendanova.com.ar/SITES/IMG/ignatech-10-2020/02-02-2021-07-02-06-logo-vector-01.png"
+          alt="redragon"
+          className="imgMarca"
+        />
+      </div>
+      <div className="footerMarcas">
+        <div className="boxTextFooter">
+          <span className="">
+            CONSULTA
+          </span>
+          <span className="spanTitleFooter">
+            EN TIENDA
+          </span>
+        </div>
+        <div className="listMarcas">
+          <ul>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+            <li className="linkMarca"><a href="/" className="linkMarca">Redragon</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Marcas;
