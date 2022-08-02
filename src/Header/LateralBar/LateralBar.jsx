e6bbb6d6bc6d71e94cd7a63406dc20d9438bc86d
Diegoo11 <ynoacamino@gmail.com>
2022-08-02 14:07:31 -0500

    new component

---
+++ src/Header/LateralBar/LateralBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './LateralBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+function LateralBar({ open, func }) {
+  return (
+    <ul className={`NavMobile ${open && 'NavMobileView'}`}>
+      <li className="li">
+        <a className="link navLink underline" href="/">Inicio</a>
+      </li>
+      <li className="li">
+        <a className="link navLink underline" href="/">Productos</a>
+      </li>
+      <li className="li">
+        <a className="link navLink underline" href="/">Marcas</a>
+      </li>
+      <li className="li">
+        <a className="link navLink underline" href="/">Tiendas</a>
+      </li>
+      <li className="li">
+        <a className="link navLink underline" href="/">Comprar</a>
+      </li>
+      <button type="button" className="btnExit pointer" onClick={func}>
+        <FontAwesomeIcon icon={faX} size="2x" />
+      </button>
+    </ul>
+  );
+}
+
+export default LateralBar;
