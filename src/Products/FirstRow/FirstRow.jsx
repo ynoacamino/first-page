db746a2dd6fd3384718167aee7fb88e08a475922
Diegoo11 <ynoacamino@gmail.com>
2023-05-07 11:03:50 -0500

    new file for module FisrtRow

---
+++ src/Products/FirstRow/FirstRow.jsx
@@ -0,0 +1,33 @@
+import './FirstRow.css';
+import React from 'react';
+
+function FirstRow() {
+  return (
+    <div className="FRBox">
+      <div className="boxGris FRLeft">
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      </div>
+
+      <div className="boxGris FRRigth">
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+      </div>
+    </div>
+  );
+}
+
+export default FirstRow;
