68d3770c530cbd2d04e53370552bafcc75f24ff2
Diegoo11 <ynoacamino@gmail.com>
2023-05-07 22:29:10 -0500

    addes new modules

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,6 +1,8 @@
 import './Products.css';
 import { React } from 'react';
 import FirstRow from './FirstRow/FirstRow';
+import SecondRow from './SecondRow/SecondRow';
+import ThirdRow from './ThirdRow/ThirdRow';
 
 function Products() {
   return (
@@ -10,7 +12,8 @@ function Products() {
       </span>
       <div className="BoxForProducts">
         <FirstRow />
-        <FirstRow />
+        <SecondRow />
+        <ThirdRow />
       </div>
     </div>
   );
