0214e7ed79daf131d1442e006775698069682c77
Diegoo11 <ynoacamino@gmail.com>
2023-07-04 15:31:21 -0500

    logic to handle load

--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 // import { Route, Routes } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import CircularProgress from '@mui/material/CircularProgress';
 import Slider from './Slider/Slider';
 import Background from './Background/Background';
 import PrimaryProducts from './PrimaryProducts/PrimaryProducts';
 import SecundaryProducts from './SecundaryProducts/SecundaryProducts';
+import { QUERY_MODULE } from '../Operations/Query';
 
 function Inicio() {
+  const { loading, data } = useQuery(QUERY_MODULE, {
+    variables: {
+      mod: 'Inicio',
+    },
+  });
+  if (loading) {
+    return (
+      <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh',
+      }}
+      >
+        <CircularProgress size={60} />
+      </div>
+    );
+  }
+
+  const images = data.findForModule.image;
+  const texts = data.findForModule.text;
+
   return (
     <>
-      <Background />
-      <Slider />
-      <PrimaryProducts />
-      <SecundaryProducts />
+      <Background images={images} texts={texts} />
+      <Slider images={images} texts={texts} />
+      <PrimaryProducts images={images} texts={texts} />
+      <SecundaryProducts images={images} texts={texts} />
     </>
+
   );
 }
 
