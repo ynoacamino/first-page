5dfad741cd4bd471f4a0a00eb706c99138d356ef
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 14:17:58 -0500

    format the data query

--- src/Products/ProductsList/ProductsList.jsx
+++ src/Products/ProductsList/ProductsList.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import './ProductsList.css';
 import Item from './Item/Item';
+import { QUERY_CATEGORY } from '../../Operations/Query';
 
-function ProductsList() {
+function ProductsList({ category = '' }) {
+  const { data, loading } = useQuery(QUERY_CATEGORY, {
+    variables: { category },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div className="ProductList">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-
+      {data.findCategory.map((item) => (
+        <Item
+          key={item.id}
+          src={item.src}
+          name={item.name}
+          cost={item.cost}
+          id={item.id}
+          brand={item.brand}
+        />
+      ))}
     </div>
   );
 }
