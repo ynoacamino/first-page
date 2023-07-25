6cac7d684241a9bb68ff563a14ac87ddb13ebb22
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 14:18:44 -0500

    add component Link to change url

--- src/Products/ProductsList/Item/Item.jsx
+++ src/Products/ProductsList/Item/Item.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import './Item.css';
+import { Link } from 'react-router-dom';
 import { ImgRatio } from '../../../util';
 
-function Item() {
+function Item({
+  name, src, brand, cost, id,
+}) {
   return (
     <div className="productItem">
-      <div className="boxImgItem">
-        <ImgRatio
-          src="https://dojiw2m9tvv09.cloudfront.net/74275/product/M_aio-lenovo-ideace-3-12338.png?66&time=1689787498"
-          alt="img"
-          aspectRatio={1}
-        />
-      </div>
-      <div className="boxText">
-        <span className="textMarcaItem">LENOVO</span>
-        <span className="textNombreItem">PC MINI ASROCK 4X4 BOX-4500U/US/AI</span>
-        <span className="textPrecioItem">S/ 2,999.0</span>
-      </div>
+      <Link className="itemLink" to={`/products/item/${id}`}>
+        <div className="boxImgItem">
+          <ImgRatio
+            src={src}
+            alt={name}
+            aspectRatio={1}
+          />
+        </div>
+        <div className="boxText">
+          <span className="textMarcaItem">{brand}</span>
+          <span className="textNombreItem">{name}</span>
+          <span className="textPrecioItem">
+            S/
+            {' '}
+            {cost}
+          </span>
+        </div>
+      </Link>
       <button type="button" className="addCardButton pointer">
         Agregar al carro
       </button>
