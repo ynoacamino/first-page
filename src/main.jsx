d767ddd4f1f280704d7979a321c5006f2ceab333
Diegoo11 <ynoacamino@gmail.com>
2023-07-25 21:29:43 -0500

    add database url

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:1111/',
+    uri: 'https://first-page-backend-production.up.railway.app/',
   }),
 });
 
