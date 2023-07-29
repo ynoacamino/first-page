742252831e770cedd7e97a5344ddf83490564256
Diegoo11 <ynoacamino@gmail.com>
2023-07-29 00:58:22 -0500

    add logic to modal box

--- src/Products/ProductsList/Item/Item.jsx
+++ src/Products/ProductsList/Item/Item.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Item.css';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import { ImgRatio } from '../../../util';
+import { useShop } from '../../../context/ShopContext';
+import { ADD_ITEM } from '../../../Operations/Mutation';
 
 function Item({
   name, src, brand, cost, id,
 }) {
+  const [upItem] = useMutation(ADD_ITEM);
+  const [modal, setModal] = useState(false);
+  const [badModal, setBadModal] = useState(false);
+  const { isLoged } = useShop();
+  const addItem = () => {
+    const loged = isLoged();
+    if (loged) setModal(true);
+    else setBadModal(true);
+
+    setTimeout(() => {
+      if (loged) setModal(false);
+      else setBadModal(false);
+    }, 3000);
+    if (loged) {
+      upItem({
+        variables: {
+          itemId: id,
+        },
+      });
+    }
+  };
+
   return (
     <div className="productItem">
       <Link className="itemLink" to={`/products/item/${id}`}>
@@ -26,9 +51,15 @@ function Item({
           </span>
         </div>
       </Link>
-      <button type="button" className="addCardButton pointer">
+      <button onClick={addItem} type="button" className="addCardButton pointer">
         Agregar al carro
       </button>
+      <div className={`boxAddItemHidden  ${modal && 'boxAddItem'}`}>
+        Producto añadido al carro.
+      </div>
+      <div className={`boxAddItemHidden  ${badModal && 'boxAddItem'}`} style={{ color: 'red' }}>
+        Primero inicia sesion
+      </div>
     </div>
   );
 }
