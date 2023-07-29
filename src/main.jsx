178cafaa79b1972718870b0eba016c3713521180
Diegoo11 <ynoacamino@gmail.com>
2023-07-29 00:51:55 -0500

    add logic to get token and headers

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,29 @@ import {
   ApolloClient, HttpLink, InMemoryCache, ApolloProvider,
 } from '@apollo/client';
 import App from './App';
+import { ShopProvider } from './context/ShopContext';
+
+const getAuth = () => {
+  const token = localStorage.getItem('user-login-token');
+  return token ? `bearer ${token}` : null;
+};
 
 const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
+    headers: {
+      authorization: getAuth(),
+    },
     uri: 'https://first-page-backend-production.up.railway.app/',
   }),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
-    <App />
+    <ShopProvider>
+      <App />
+    </ShopProvider>
   </ApolloProvider>
   ,
 );
